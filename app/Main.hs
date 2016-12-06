f96091699bbfe5ee4967391dfea03007fe5ab0f1
Werner <werner_a_e@yahoo.es>
2016-12-06 16:25:29 -0400

    add cors

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import           Data.Monoid                 ((<>))
+import           Network.Wai.Middleware.Cors
+import qualified Data.ByteString.Char8       as C
+import qualified Data.CaseInsensitive        as CI
+import           Network.Wai                 (Application)
 import           Database.Persist.Postgresql (runSqlPool)
 import           Network.Wai.Handler.Warp    (run)
 import           System.Environment          (lookupEnv)
@@ -12,10 +17,29 @@ import           Config                      (Config (..), Environment (..), get
 import           Models                      (doMigrations)
 import           Safe                        (readMay)
 
+
 main :: IO ()
 main = do
     cfg  <- getConfig
     port <- getPort
     let logger = setLogger $ getEnv cfg
     runSqlPool doMigrations $ getPool cfg
-    run port $ logger $ app cfg
+    run port $ logger $ stack $ app cfg
+
+stack :: Application -> Application
+stack = cors'
+  where
+    cors' = cors (const $ Just corsResourcePolicy)
+
+corsResourcePolicy :: CorsResourcePolicy
+corsResourcePolicy = CorsResourcePolicy
+  { corsOrigins        = Nothing
+  , corsMethods        = [C.pack "GET",  C.pack "HEAD", C.pack "OPTIONS", 
+                          C.pack "POST", C.pack "PUT",  C.pack "DELETE"]
+  , corsRequestHeaders = simpleResponseHeaders <> [CI.mk $ C.pack "Authorization"]
+  , corsExposedHeaders = Nothing
+  , corsMaxAge         = Nothing
+  , corsVaryOrigin     = False
+  , corsRequireOrigin  = False
+  , corsIgnoreFailures = False
+  }
