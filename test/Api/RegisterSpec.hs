4f0dffb269421b870994cbbd95fba6282f9a44b3
Werner <werner_a_e@yahoo.es>
2017-01-06 12:49:32 -0400

    add register spec

---
+++ test/Api/RegisterSpec.hs
@@ -0,0 +1,46 @@
+
+module Api.RegisterSpec (main, spec) where
+
+import qualified Data.ByteString.Char8            as C
+import           SpecSupport
+
+import           Models.Base
+import           Models.Register
+import           Api.Register
+
+registerPost    :: RegisterUser -> Manager -> BaseUrl -> ClientM String
+confirmationGet :: String -> String -> Manager -> BaseUrl -> ClientM String
+registerPost :<|> confirmationGet = client apiSpec
+
+apiSpec :: Proxy Api.Register.API
+apiSpec = Proxy
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = with appSpec $ do
+    describe "/register" $ do
+        it "Register" $ do
+          let user = RegisterUser "test@test.com"
+                                  (Just "Test")
+                                  (Just "Super Test")
+                                  (Just "My Company")
+                                  "123456"
+                                  "123456"
+          postJson (C.pack "/register") user `shouldRespondWith` 200
+
+type APISpec = Api.Register.API
+
+serverSpec :: ServerT APISpec App
+serverSpec = Api.Register.server
+
+appToServerSpec :: Config -> Server APISpec
+appToServerSpec cfg = enter (convertApp cfg) serverSpec
+
+appSpec :: IO Application
+appSpec = do
+    setupDB
+    pool <- makePool Test
+    let cfg = Config { getPool = pool, getEnv = Test }
+    return $ serveWithContext apiSpec authServerContextSpec (appToServerSpec cfg)
